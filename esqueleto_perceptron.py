1b81a97c6adee06f6a05360fdcdd986a1388eda2
SamDino <samuel.cas.96@gmail.com>
2017-03-02 02:42:01 -0600

    Def de funciones

--- esqueleto_perceptron.py
+++ esqueleto_perceptron.py
@@ -3,7 +3,7 @@
  Fac. de Ingenieria Mecanica y Electrica
  Administracion y Sistemas
  Ingenieria en Tecnologias de Software
- 
+
  Programacion de Sistemas Adaptativos
  Redes neuronales: Perceptron
  Programa un perceptron para que clasifique el suelo en apto o no apto para la agricultura
@@ -16,23 +16,23 @@
           entrada - lista con las caracteristicas del ejemplo a procesar
   Salida: suma de la multiplicacion de cada peso por cada caracteristica
 '''
-#def funcion_nucleo(pesos, entrada):
-#PROGRAMA AQUI LA FUNCION DE NUCLEO (QUITA EL "#" DE LA LINEA ANTERIOR)
+def funcion_nucleo(pesos, entrada):
+# Cada entrada en esta funcion.
 
 '''
   Recibe: resultado_nucleo - valor que representa el resultado de la funcion de nucleo (g(X))
   Salida: devuelve 1 si resultado_nucleo es mayor o igual a cero; en caso contrario, devuelve un 0
 '''
-#def funcion_activacion(resultado_nucleo):
-#PROGRAMA AQUI LA FUNCION DE ACTIVACION (QUITA EL "#" DE LA LINEA ANTERIOR)
+def funcion_activacion(resultado_nucleo):
+# Cada salida en esta funcion.
 
-		
+
 max_iter=50
 tasa_aprendizaje=0.1
 pesos=[1,0,0,0,0,0,0]
-entradas=[[[-1,1,0,1,0,0,0], 1], [[-1,1,0,1,1,0,0], 1], [[-1,1,0,1,0,1,0], 1] 
-, [[-1,1,1,0,0,1,1], 1], [[-1,1,1,1,1,0,0], 1], [[-1,1,0,0,0,1,1], 1], [[-1,1,0,0,0,1,0], 0] 
-, [[-1,0,1,1,1,0,1], 1], [[-1,0,1,1,0,1,1], 0], [[-1,0,0,0,1,1,0], 0], [[-1,0,1,0,1,0,1], 0], 
+entradas=[[[-1,1,0,1,0,0,0], 1], [[-1,1,0,1,1,0,0], 1], [[-1,1,0,1,0,1,0], 1]
+, [[-1,1,1,0,0,1,1], 1], [[-1,1,1,1,1,0,0], 1], [[-1,1,0,0,0,1,1], 1], [[-1,1,0,0,0,1,0], 0]
+, [[-1,0,1,1,1,0,1], 1], [[-1,0,1,1,0,1,1], 0], [[-1,0,0,0,1,1,0], 0], [[-1,0,1,0,1,0,1], 0],
 [[-1,0,0,0,1,0,1], 0], [[-1,0,1,1,0,1,1], 0], [[-1,0,1,1,1,0,0], 0]]
 
 iter=0
@@ -43,10 +43,10 @@ pincorrectos=1.0
 #while(pincorrectos>0.2 and iter<max_iter):
 
 	#incorrectos=0
-	
+
 
 	#AQUI VA EL CODIGO PARA CALCULAR LA SALIDA (y)
-	
+
 		#if y!=d: #Checa que esta linea esta mas identada que el resto (cuidado ¡!)
 		#	incorrectos=incorrectos+1
 		#	print("ACTUALIZANDO PESOS")
